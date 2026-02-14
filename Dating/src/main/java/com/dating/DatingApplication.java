b34f62697c5afbfe5d240cfa26f03d3013d7e0d3
jack0028 <131895221+jack0028@users.noreply.github.com>
2026-02-14 23:00:50 +0530

    Update DatingApplication.java

--- Dating/src/main/java/com/dating/DatingApplication.java
+++ Dating/src/main/java/com/dating/DatingApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class DatingApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
-        SpringApplication.run(DatingAppApplication.class, args);
+        SpringApplication.run(DatingApplication.class, args);
     }
 }
 
