bf2e21e036a0ff66ee197f67d3615e81749d31c7
jack0028 <131895221+jack0028@users.noreply.github.com>
2026-02-15 14:38:03 +0530

    Update DatingApplicationTests.java

--- Dating/src/test/java/com/dating/DatingApplicationTests.java
+++ Dating/src/test/java/com/dating/DatingApplicationTests.java
@@ -1,6 +1,7 @@
 package com.dating;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test; 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
